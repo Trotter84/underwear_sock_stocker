d8f1dd2a800d89cb6eeeb6c27adee494166ff74f
Daniel Trotter <flare84@gmail.com>
2020-07-24 14:43:33 -0600

    counter is working

--- src/components/Socks.js
+++ src/components/Socks.js
@@ -2,13 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-class Socks extends Component {
+class Socks extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  reset() {
+    this.setState({
+      count: 0
+    });
+  };
+
   render() {
-    return(
+    return (
       <div className='split bottom' id='bottomSocks'>
         <h1>Socks</h1>
+        <button className='dec' onClick={(e) => this.decrement(e)}>-</button>
+        <button className='inc' onClick={(e) => this.increment(e)}>+</button>
+        <button className='reset' onClick={(e) => this.reset(e)}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
       </div>
-    )
+    );
   }
 };
 
--- src/components/Underwear.js
+++ src/components/Underwear.js
@@ -2,14 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-class Underwear extends Component {
+class Underwear extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  reset() {
+    this.setState({
+      count: 0
+    });
+  };
+
   render() {
-    return(
+    return (
       <div className='split top' id='topUnderwear'>
         <h1>Underwear</h1>
+        <button className='dec' onClick={(e) => this.decrement(e)}>-</button>
+        <button className='inc' onClick={(e) => this.increment(e)}>+</button>
+        <button className='reset' onClick={(e) => this.reset(e)}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
       </div>
-    )
+    );
   }
-}
+};
 
 export default Underwear;
